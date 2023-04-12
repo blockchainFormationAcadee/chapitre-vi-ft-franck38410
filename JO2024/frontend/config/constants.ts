09d9c1cd8434c18cef575394910a5c2bd5488d27
Atos Franck <franck.petretto@atos.net>
2023-04-12 20:37:56 +0200

    ajout du no de contrat pour Netify

--- JO2024/frontend/config/constants.ts
+++ JO2024/frontend/config/constants.ts
@@ -1,4 +1,4 @@
 export const apiKeyAlchemyProvider = 'Y3mDOtUzfEkHU8KaxuyTHKncgaR5Xibb';
-export const contractAddress = '0x3e063382d0F2AFb43A75a0FE2283c64086Cc8C86';
+export const contractAddress = '0x0B48314F751c904e1A9b278f762ba01a5d2ABa1a';
 export const formAcadeeAddress = '0x005D71CA579843a1C3EeFEd02E5909CF77976761';
 export const formAcadeeAddress2 = '0x7Cd33a833dC720Acc8d17bC17edC41cc526FebB2';
