06a48036c003d432114d2cbbc8470c5759cdec69
Atos Franck <franck.petretto@atos.net>
2023-04-18 17:11:57 +0200

    Version de démo address du contrat 0x6143858f106fb78Ea3996BEbb7386C2d0b1d4e48

--- JO2024/frontend/config/constants.ts
+++ JO2024/frontend/config/constants.ts
@@ -1,4 +1,4 @@
 export const apiKeyAlchemyProvider = 'Y3mDOtUzfEkHU8KaxuyTHKncgaR5Xibb';
-export const contractAddress = '0x55a82C338B9863815aEdeAD2EcE7963850dedb12';
+export const contractAddress = '0x6143858f106fb78Ea3996BEbb7386C2d0b1d4e48';
 export const formAcadeeAddress = '0x005D71CA579843a1C3EeFEd02E5909CF77976761';
 export const formAcadeeAddress2 = '0x7Cd33a833dC720Acc8d17bC17edC41cc526FebB2';
